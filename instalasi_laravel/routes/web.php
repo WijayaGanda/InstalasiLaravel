3d5167d978c8f9ff251694cdde33d8b7af48f153
WijayaGanda <64698425+WijayaGanda@users.noreply.github.com>
2021-08-03 07:58:59 +0700

    Route Laravel

--- instalasi_laravel/routes/web.php
+++ instalasi_laravel/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/profil', function () {
+    return view('profil');
+});
+
+Route::get('/galeri', function () {
+    return view('galeri');
+});
